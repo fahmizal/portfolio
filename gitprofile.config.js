fc4570e115413925f89bd9465f4f0e3e63919a03
Fahmizal <fahmizal@ugm.ac.id>
2023-06-09 10:11:44 +0700

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -29,7 +29,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/fahmizal/portfolio/blob/main/Fahmizal_CV.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Control System',
